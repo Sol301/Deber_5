86d77102beac0e53044f99558de1376dd232057a
USER1 <USER1@USER>
2018-10-11 13:54:20 -0500

    Mostrara el mensaje Deber_5

---
+++ src/ec/edu/ister/vista/Aplicacion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ister.vista;
+
+/**
+ *
+ * @author USER1
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        System.out.println("DEBER PRACTICO 5");
+    }
+}
